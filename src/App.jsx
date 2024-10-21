61a62474e7182364c98dba3217b37992e41fcc92
mubashirbappi <mubashirbappi@gmail.com>
2024-10-21 22:45:45 +0600

    load data is ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
+import Blogs from './component/blogs'
+import Headers from './component/Headers'
 
 function App() {
   
   return (
     <>
+    <Headers/>
     <h3 className='text-2xl font-medium text-rose-600 text-center'>hi cheaking talwind</h3>
-     
+     <Blogs/>
     </>
   )
 }
---
+++ src/component/Headers.jsx
@@ -0,0 +1,12 @@
+
+
+const Headers = () => {
+    return (
+        <div>
+            <h1>hlw world</h1>
+            
+        </div>
+    );
+};
+
+export default Headers;
