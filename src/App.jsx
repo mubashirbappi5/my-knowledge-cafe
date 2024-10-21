324bce3fb1dae45917638cc784977234244d5944
mubashirbappi <mubashirbappi@gmail.com>
2024-10-21 22:07:09 +0600

    projects setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <h3 className='text-2xl font-medium text-rose-600 text-center'>hi cheaking talwind</h3>
+     
+    </>
+  )
+}
+
+export default App
